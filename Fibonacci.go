5167714117c2c41f199d36175b05051537e188cd
Shuddha Chowdhury <shuddho.aiub@gmail.com>
2020-09-28 12:03:08 -0500

    Added Fibonacci Sequence

---
+++ Fibonacci.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) int {
+	x := 0
+	y := 1
+	// We iterate until we get the desired position in the sequence
+	for i := 0; i < n; i++ {
+		// Using temporary variable to swap the variables
+		temp := x
+		x = y
+		y = temp + x
+	}
+	return x
+}
+
+func main() {
+	// Showing first 30 fibonacci numbers here
+	for p := 0; p < 30; p++ {
+		// Then we compute the numbers and display the result
+		displayResult := fibSequence(p)
+		fmt.Printf("Fibonacci number %v = %v\n", p, displayResult)
+	}
+}
